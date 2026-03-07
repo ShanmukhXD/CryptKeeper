fbc6be6cbaf068c6c320e25700787324ae289fad
Shanmukh Das <shanmukhxd@gmail.com>
2026-03-07 13:10:47 +0530

    Added basic XOR Encryption Added password generator Fixed bugs

--- src/CryptKeeper.java
+++ src/CryptKeeper.java
@@ -1,36 +1,32 @@
+import javax.swing.*;
 import java.util.Scanner;
 import java.io.*;
 
 public class CryptKeeper {
     static Scanner sc = new Scanner(System.in);
-    static CryptKeeper keep =  new CryptKeeper();
-    static FileReader fr;
-    static {
-        try {
-            fr = new FileReader("CryptKeeperData.txt");
-        } catch (FileNotFoundException e) {
-            System.out.println("Error: File not found");
-        }
-    }
-    static BufferedReader br = new BufferedReader(fr);
+    static CryptKeeper keep = new CryptKeeper();
+    String masterPassword;
     void authorize() throws IOException {
+        BufferedReader mbr = new BufferedReader(new FileReader("master.txt"));
         System.out.println("Enter master password: ");
-        String masterPassword = sc.nextLine();
-        String retrieveMaster = " ";
-        while((retrieveMaster = br.readLine()) != null){
-            if(retrieveMaster.equals(masterPassword)){
-                keep.menu();
-            } else
-                System.out.println("Wrong password");
+        masterPassword = sc.nextLine();
+        String retrieveMaster = mbr.readLine();
+        if(retrieveMaster!=null&&retrieveMaster.equals(masterPassword)){
+            keep.menu();
+        }else{
+            System.out.println("Wrong password");
         }
+        mbr.close();
     }
     void menu() throws IOException{
         System.out.println("Welcome to CryptKeeper \n---Menu---");
         System.out.println("1. Add password");
         System.out.println("2. View password");
         System.out.println("3. Delete password");
-        System.out.println("4. Exit");
+        System.out.println("4. Generate strong password");
+        System.out.println("5. Exit");
         int ch = sc.nextInt();
+        sc.nextLine();
         switch(ch){
             case 1:
                 keep.add();
@@ -42,7 +38,11 @@ public class CryptKeeper {
                 keep.delete();
                 break;
             case 4:
+                keep.generate();
+                break;
+            case 5:
                 System.exit(0);
+                break;
             default:
                 System.out.println("Invalid choice number");
         }
@@ -53,28 +53,95 @@ public class CryptKeeper {
         System.out.println("Enter username: ");
         String username = sc.nextLine();
         System.out.println("Enter password: ");
-        String  password = sc.nextLine();
-
-        FileWriter fw = new FileWriter("CryptKeeperData.txt", true);
-        fw.write(site + "|" + username + "|" + password + "\n");
+        String password = sc.nextLine();
+        String key = masterPassword, encrypted = "";
+        int keyIndex = 0;
+        for(int i = 0; i<password.length(); i++){
+            keyIndex = i%key.length();
+            char keyChar = key.charAt(keyIndex);
+            encrypted += (char)(password.charAt(i)^keyChar);
+        }
+        System.out.println("Saved Successful!");
+        FileWriter fw = new FileWriter("CryptKeeperData.txt",true);
+        fw.write(site+"|"+username+"|"+encrypted+"\n");
+        fw.close();
     }
     void view() throws IOException{
-        String line  = br.readLine();
-        String data[] = line.split("\\|");
-        System.out.print(data[0] + "|" + data[1] + "|" + data[2]);
+        BufferedReader br = new BufferedReader(new FileReader("CryptKeeperData.txt"));
+        String line = " ", key = masterPassword;
+        int keyIndex = 0;
+        while((line=br.readLine())!=null){
+            String decrypted = "";
+            String data[]=line.split("\\|");
+            for(int i = 0; i<data[2].length(); i++){
+                char ch = data[2].charAt(i);
+                keyIndex = i%key.length();
+                char keyChar = key.charAt(keyIndex);
+                decrypted += (char)(ch^keyChar);
+            }
+            System.out.println(data[0]+"|"+data[1]+"|"+decrypted);
+        }
+        br.close();
     }
-    void  delete() throws IOException{
+    void delete() throws IOException{
+        BufferedReader br = new BufferedReader(new FileReader("CryptKeeperData.txt"));
         BufferedWriter bw = new BufferedWriter(new FileWriter("temp.txt"));
-        String line = " ";
+        String line=" ";
         System.out.println("Enter site to delete: ");
         String target = sc.nextLine();
-        while((line=br.readLine()) != null){
-            String[] data = line.split("\\|");
-            String site = data[0];
+        while((line=br.readLine())!=null){
+            String[] data=line.split("\\|");
+            String site=data[0];
             if(!site.equals(target))
-                bw.write(line + "\n");
+                bw.write(line+"\n");
         }
-        new File("vault.txt").delete();
-        new File("temp.txt").renameTo(new File("vault.txt"));
+        br.close();
+        bw.close();
+        new File("CryptKeeperData.txt").delete();
+        new File("temp.txt").renameTo(new File("CryptKeeperData.txt"));
+    }
+    void generate() throws IOException {
+        String allChars = " ";
+        for(int i = 48; i<=57; i++)
+            allChars += (char)i;
+        for(int i = 65; i<=90; i++)
+            allChars += (char)i;
+        for(int i = 97; i<=122; i++)
+            allChars += (char)i;
+        allChars += "!@#$%^&*";
+        System.out.println("Enter password length: ");
+        int length = sc.nextInt();
+        int randomIndex = 0;
+        String password =" ";
+        //Generate:
+        for(int i = 0; i<length; i++){
+            randomIndex = (int)(Math.random()*allChars.length());
+            char ch = allChars.charAt(randomIndex);
+            password += ch; //append
+        }
+        System.out.println("Generated password: "+password);
+        System.out.println("Save this password? ");
+        String ch = sc.nextLine().toUpperCase();
+        if(ch.equals("Y") || ch.equals("YES")){
+            System.out.println("Enter site: ");
+            String site = sc.nextLine();
+            System.out.println("Enter username: ");
+            String username = sc.nextLine();
+            String key = masterPassword,  encrypted = " ";
+            int keyIndex = 0;
+            for(int i = 0; i<password.length(); i++){
+                keyIndex = i%key.length();
+                char keyChar = key.charAt(keyIndex);
+                encrypted += (char)(password.charAt(i)^keyChar);
+            }
+            FileWriter fw = new FileWriter("CryptKeeperData.txt", true);
+            fw.write(site + "|" + username + "|" + encrypted + "\n");
+            fw.close();
+        }
+        else
+            System.exit(0);
+    }
+    public static void main(String[] args) throws IOException {
+        keep.authorize();
     }
 }
